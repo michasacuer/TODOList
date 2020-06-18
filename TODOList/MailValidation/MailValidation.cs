3cb565c5706f79dbb3324d5076e4e70c8ad69e7e
michasacuer <michasacuer3@gmail.com>
2020-06-18 19:14:36 +0200

    MailValidation refactoring

--- TODOList/MailValidation/MailValidation.cs
+++ TODOList/MailValidation/MailValidation.cs
@@ -10,15 +10,8 @@ namespace TODOList
         /// <param name="inputEmail"></param>
         /// <returns>True if mail is valid otherwise false</returns>
         public static bool IsEmailValid(string inputEmail)
-        {
-            string strRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
-                  @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
-                  @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
-            Regex re = new Regex(strRegex);
-            if (re.IsMatch(inputEmail))
-                return (true);
-            else
-                return (false);
-        }
+            =>  new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                          @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                          @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$").IsMatch(inputEmail);
     }
 }
